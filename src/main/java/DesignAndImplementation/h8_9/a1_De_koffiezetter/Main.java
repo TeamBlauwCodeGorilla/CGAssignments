92f95fd1c68a25384eab8df7bc53d83c717e5923
miked <mikedegroot1999@hotmail.com>
2022-10-11 10:03:48 +0200

    Initial commit with h8 & h9's first assignment De koffiezetter

---
+++ src/main/java/DesignAndImplementation/h8_9/a1_De_koffiezetter/Main.java
@@ -0,0 +1,8 @@
+package DesignAndImplementation.h8_9.a1_De_koffiezetter;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: Implement the assignment: 'De koffiezetter' using inheritance and abstraction of classes and interfaces.
+        // Tip: Do this on your own branch containing your first name.
+    }
+}
