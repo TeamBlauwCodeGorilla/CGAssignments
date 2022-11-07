9dd07c283ae4152f1114e2578d6dec1a83b29061
miked <mikedegroot1999@hotmail.com>
2022-11-07 14:42:30 +0100

    feat: new assignment folder for 'Documenten Bijhouden'

---
+++ src/main/java/design_and_implementation/h8_9/a2_documenten_bijhouden/Main.java
@@ -0,0 +1,9 @@
+package design_and_implementation.h8_9.a2_documenten_bijhouden;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO: Implement the assignment: 'Documenten Bijhouden'
+        // Tip: Do this on your own branch containing your first name.
+    }
+}
